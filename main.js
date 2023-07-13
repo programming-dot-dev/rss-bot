662d44a88e1461e2cfca017297ad8f3f3a83f1a6
th3raid0r <brian@th3rogers.com>
2023-07-12 21:18:31 -0700

    Small fix on cron expression

--- main.js
+++ main.js
@@ -208,7 +208,7 @@ const bot = new LemmyBot.LemmyBot({
     },
     schedule: [
         {
-            cronExpression: '0 */5 * * * *',
+            cronExpression: '0 */20 * * * *',
             timezone: 'America/Toronto',
             doTask: async ({getCommunityId, createPost}) => {
                 console.log(`${chalk.green('STARTED:')} RSS Feed Fetcher.`);
